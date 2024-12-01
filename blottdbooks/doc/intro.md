b3824ad8188969ca5866fb96f16e125d85acd4e3
Melvin Ray <positivemray@gmail.com>
2024-12-01 15:04:26 -0600

    created leiningen project

---
+++ blottdbooks/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to blottdbooks
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ blottdbooks/test/blottdbooks/core_test.clj
@@ -0,0 +1,7 @@
+(ns blottdbooks.core-test
+  (:require [clojure.test :refer :all]
+            [blottdbooks.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
